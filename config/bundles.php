018f5a4c0e88705b472bad358592fc4a1cc49390
Den <swanden@gmail.com>
2021-09-24 15:50:22 +0300

    Added NelmioCorsBundle for CORS

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
---
+++ src/Serializer/ErrorNormalizer.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Serializer;
+
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class ErrorNormalizer // implements NormalizerInterface
+{
+    public function normalize($exception, string $format = null, array $context = [])
+    {
+        return [
+            'error' => [
+                'message' => $exception->getMessage(),
+                'code' => $exception->getStatusCode(),
+            ]
+        ];
+    }
+
+    public function supportsNormalization($data, string $format = null)
+    {
+        return $data instanceof FlattenException;
+    }
+}
